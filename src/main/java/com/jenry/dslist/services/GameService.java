555a89dc50e9f8e806600fe9cb985c08af3e73b9
jenry <Jenrycn02@outlook.com>
2023-05-12 21:16:26 -0300

    endpoint findById for game

--- src/main/java/com/jenry/dslist/services/GameService.java
+++ src/main/java/com/jenry/dslist/services/GameService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jenry.dslist.dto.GameDTO;
 import com.jenry.dslist.dto.GameMinDTO;
@@ -16,11 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public GameDTO findById(Long id) {
 		Game result = gameRepository.findById(id).get();
-		return new GameDTO(result);
+		GameDTO dto = new GameDTO(result);
+		return dto;
 	}
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result  = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
